c14721f64c9418ac6768c31299f0614189e840ae
taiti09 <taitiabe1997@gmail.com>
2022-12-31 17:22:34 +0900

    行列計算

---
+++ std/calcMatrix.go
@@ -0,0 +1,26 @@
+// You can edit this code!
+// Click here and start typing.
+package std
+
+import "fmt"
+
+func main() {
+	
+}
+
+func CalcMatrix(x *[][]int, y *[][]int) *[][]int {
+
+	var res [][]int
+	
+	N := len([](*x))
+	M := len([](*y))
+	
+	res = make(int,0,10)
+	[]res =make([]int,0,10)
+		
+	for i := 0; i < N; i++ {
+		for j := 0; j < M; j++ {
+			res 
+		}
+	}	
+}
